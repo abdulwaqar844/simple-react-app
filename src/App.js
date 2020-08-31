1d0d5cfe453812bfff9035c932dfc98e0172dd54
abdulwaqar844 <abdulwaqar844@users.noreply.github.com>
2020-08-31 21:24:02 +0500

    First Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <body>
+    <button class="" > Click Me</button>
+    </body>
+  );
+}
+export default App;
---
+++ src/import.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function Apps() {
+  return (<body>
+    <ol>
+      <li>WordPress</li>
+      <li>Java</li>
+      <li>Graphic Desgin </li>
+      <li>Adobe PhotoShop </li>
+      <li>Adobe Illustrator </li>
+      <li>Python</li>
+
+    </ol></body>
+  );
+}
+export default Apps;
